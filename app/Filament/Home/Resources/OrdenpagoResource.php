ef366ee7fd023fdd5a911c0a2ce42a57490ae532
Giusti89 <giustiever17@gmail.com>
2025-08-19 10:19:57 -0400

    correccion orden de pago2

--- app/Filament/Home/Resources/OrdenpagoResource.php
+++ app/Filament/Home/Resources/OrdenpagoResource.php
@@ -29,14 +29,6 @@ class OrdenpagoResource extends Resource
     protected static ?int $navigationSort = 3;
 
 
-    public static function getEloquentQuery(): Builder
-    {
-        return parent::getEloquentQuery()
-            ->whereHas('cliente', function ($query) {
-                $query->where('usuario_id', auth()->id());
-            });
-    }
-
     public static function form(Form $form): Form
     {
         return $form
