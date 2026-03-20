c618d90e16bad50168d428ad123cfadcbe309346
Giusti89 <giustiever17@gmail.com>
2026-03-20 18:32:10 -0400

    se agrego los soft deletes

--- app/Filament/Home/Resources/MovimientoahorroResource.php
+++ app/Filament/Home/Resources/MovimientoahorroResource.php
@@ -131,7 +131,7 @@ class MovimientoahorroResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    
+                    Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
