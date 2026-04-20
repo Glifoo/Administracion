2fa4dc9bd974fe449e4030321ba9bad732d356d4
Giusti89 <giustiever17@gmail.com>
2026-04-20 13:36:48 -0400

    correccion de tablas  transaccion

--- app/Filament/Home/Resources/MovimientoahorroResource.php
+++ app/Filament/Home/Resources/MovimientoahorroResource.php
@@ -137,9 +137,7 @@ class MovimientoahorroResource extends Resource
 
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\BulkActionGroup::make([]),
             ]);
     }
 
