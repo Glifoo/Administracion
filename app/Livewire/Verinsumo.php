9eebab857f18aaa27bb4bb046d6daa090daf4aaa
Giusti89 <giustiever17@gmail.com>
2026-04-23 17:22:33 -0400

    actualizacion query insumos vista

--- app/Livewire/Verinsumo.php
+++ app/Livewire/Verinsumo.php
@@ -9,10 +9,41 @@ use Livewire\Component;
 class Verinsumo extends Component
 {
     public $identificador;
+    public $trabajo;
+    public $insumos;
+    public $totalInsumos;
+
+    public function mount()
+    {
+        $this->cargarDatos();
+    }
+
+    protected function cargarDatos()
+    {
+        // Una sola consulta optimizada
+        $this->trabajo = Trabajo::with([
+            'insumos' => function ($query) {
+                $query->with('medida')  // Eager loading de medida
+                    ->orderBy('nombre')
+                    ->select('id', 'nombre', 'detalle', 'costo', 'cantidad', 'trabajo_id');
+            },
+            'cliente:id,nombre' // Si necesitas datos del cliente
+        ])->findOrFail($this->identificador);
+
+        $this->insumos = $this->trabajo->insumos;
+
+        // Calcular totales si es necesario
+        $this->totalInsumos = $this->insumos->sum(function ($insumo) {
+            return $insumo->costo ;
+        });
+    }
+
     public function render()
     {
-         $insumos = Insumo::where('trabajo_id', $this->identificador)->get();
-          $trabajo = Trabajo::find($this->identificador);
-        return view('livewire.verinsumo',compact('insumos','trabajo'));
+        return view('livewire.verinsumo', [
+            'insumos' => $this->insumos,
+            'trabajo' => $this->trabajo,
+            'totalInsumos' => $this->totalInsumos ?? 0
+        ]);
     }
 }
